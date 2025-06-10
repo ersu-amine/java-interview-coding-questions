012ae2fdc0600ce60ddcca18f143a30ebebc4fac
ersu-amine <ersu.amine@gmail.com>
2025-06-10 00:28:49 -0400

    Interview Question return single copy of duplicated characters in a string

---
+++ src/test/java/com/aersu/interview_questions/FindDuplicateChars.java
@@ -0,0 +1,41 @@
+package com.aersu.interview_questions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class FindDuplicateChars {
+    /*
+    Find duplicate char from string. Write a program to find duplicate characters in a given String.
+
+        Input: hackerearth
+        Output: h a e r
+     */
+
+    public static String duplicatedCharacters(String str) {
+        str=str.toLowerCase();
+
+        Set<String> characters = new HashSet<>(); //for containing all characters, no duplicates
+        Set<String> duplicates = new HashSet<>(); //for duplicated chars, will have single copy
+
+        for (String letter : str.split("")) {
+
+            if (!characters.contains(letter)) {
+                characters.add(letter);
+            } else {
+                duplicates.add(letter);
+            }
+        }
+
+        //The new String[0] is a common idiom — it tells toArray() what type of array to create.
+        //otherwise it will return Object[]
+        String[] array = duplicates.toArray(new String[0]);
+        System.out.println(String.join("", array));
+    }
+
+    @Test
+    void test() {
+        Assertions.assertEquals("pa",duplicatedCharacters("papatya"));
+    }
+}
