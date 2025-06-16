aac9a83b12df8974f9ad9dbb4982cf71341a0899
ersu-amine <ersu.amine@gmail.com>
2025-06-16 00:25:21 -0400

    Interview Question second largest and second smallest numbers in an integer array

--- src/test/java/com/aersu/interview_questions/SecondLargestSmallest.java
+++ src/test/java/com/aersu/interview_questions/SecondLargestSmallest.java
@@ -14,14 +14,46 @@ public class SecondLargestSmallest {
 
         Arrays.sort(numbers);
         array[0] = numbers[1];// second smallest number at index 1
-        array[1] = numbers[numbers.length-2]; //second largest num at index max-1
+        array[1] = numbers[numbers.length - 2]; //second largest num at index max-1
         return array;
     }
 
+    public static int[] secondMinMaxWithLoop(int[] numbers) {
+        int[] array = new int[2]; //to store second largest and smallest
+
+        //if built in methods are not allowed, use loop
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        for (int i : numbers) {
+            if (i > max) {
+                secondMax = max;
+                max = i;
+            } else if (i > secondMax) {
+                secondMin = i;
+            }
+
+            if (i < min) {
+                secondMin = min;
+                min = i;
+            } else if (i < secondMin) {
+                secondMin = i;
+            }
+        }
+        array[0] = secondMin;
+        array[1] = secondMax;
+
+        return array;
+    }
+
+
     @Test
     void test() {
-        int[] num = {2,3,8,10,5,1,9,4};
+        int[] num = {2, 3, 8, 10, 5, 1, 9, 4};
 
         System.out.println(Arrays.toString(secondMinMax(num)));
+        System.out.println(Arrays.toString(secondMinMaxWithLoop(num)));
     }
 }
